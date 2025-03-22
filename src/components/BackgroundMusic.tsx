d97f2f708c960d9571e9ff38acf71dc9c84fba13
Luis Caraballo <lcm7039@gmail.com>
2025-03-22 08:52:19 -0500

    a

--- src/components/BackgroundMusic.tsx
+++ src/components/BackgroundMusic.tsx
@@ -8,15 +8,16 @@ const BackgroundMusic = () => {
                 pointerEvents: "none"
             }}
         >
-            <iframe 
-                id="youtube-player"
-                width="560" 
-                height="315" 
-                src="https://www.youtube.com/watch?v=dXNeYHDhqbg&list=RDdXNeYHDhqbg&start_radio=1&ab_channel=reikVEVO?autoplay=1&mute=1&loop=1" 
-                frameBorder={0} 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                allowFullScreen>
-            </iframe>
+            <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/dXNeYHDhqbg?si=6_gLLkqPuIUVduna"
+                title="YouTube video player"
+                frameBorder={0}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+            ></iframe>
         </div>
     );
 };
