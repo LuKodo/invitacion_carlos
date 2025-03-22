606fc707ad8df976702bc43d5be2b6a49259bbc9
Luis Caraballo <lcm7039@gmail.com>
2025-03-22 09:22:44 -0500

    po

--- src/components/BackgroundMusic.tsx
+++ src/components/BackgroundMusic.tsx
@@ -1,23 +1,14 @@
 const BackgroundMusic = () => {
     return (
         <div
-            className="position-absolute overflow-hidden opacity-0"
+            className="d-none"
             style={{
                 width: "1",
                 height: "1",
                 pointerEvents: "none"
             }}
         >
-            <iframe
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/dXNeYHDhqbg?autoplay=1&loop=1"
-                title="YouTube video player"
-                frameBorder={0}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-            ></iframe>
+            <iframe src="https://invitacioncarlosykelly.com.co/media/Intro.mp3" width="640" height="480" allow="autoplay"></iframe>
         </div>
     );
 };
