d7b8ed6e5d765b418a4e9d7f4570c6dd66bc712c
Luis Caraballo <lcm7039@gmail.com>
2025-03-22 09:04:58 -0500

    carlos music

--- src/components/BackgroundMusic.tsx
+++ src/components/BackgroundMusic.tsx
@@ -11,7 +11,7 @@ const BackgroundMusic = () => {
             <iframe
                 width="560"
                 height="315"
-                src="https://www.youtube.com/embed/dXNeYHDhqbg?si=6_gLLkqPuIUVduna?autoplay=1&loop=1"
+                src="https://www.youtube.com/embed/dXNeYHDhqbg?autoplay=1&loop=1"
                 title="YouTube video player"
                 frameBorder={0}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
