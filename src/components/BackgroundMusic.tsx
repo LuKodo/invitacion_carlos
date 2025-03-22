49c5fbebbf00154fe3c7847acd221a8e9b7c447f
Luis Caraballo <lcm7039@gmail.com>
2025-03-22 09:29:28 -0500

    wewfdsasd

--- src/components/BackgroundMusic.tsx
+++ src/components/BackgroundMusic.tsx
@@ -8,9 +8,7 @@ const BackgroundMusic = () => {
                 pointerEvents: "none"
             }}
         >
-            <audio controls autoPlay>
-                <source src="https://invitacioncarlosykelly.com.co/media/Intro.mp3" type="audio/mpeg"></source>
-            </audio>
+            <iframe id="player-musica-fondo" frameBorder="0" allowFullScreen allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" title="Ed Sheeran - Perfect (Lyrics)" width="10" height="10" src="https://www.youtube.com/embed/?playlist=EH30RAXnRWs&amp;loop=1&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fwww.fixdate.io&amp;widgetid=1&amp;forigin=https%3A%2F%2Fwww.fixdate.io%2Fco%2Fmodelo-invitacion%2Fhojas&amp;aoriginsup=1&amp;vf=1"></iframe>
         </div>
     );
 };
